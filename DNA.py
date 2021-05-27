81d4abb77e2ebb682c66b31a00053a4693de163d
pawlo555 <spawel555@outlook.com>
2021-05-27 19:58:16 +0200

    Description

---
+++ DNA.py
@@ -0,0 +1,9 @@
+"""
+A DNA can be an array of floats with values from 0 to 1.
+First 2 are the coordinates of first triangle points, second 2 for second point and third for third.
+Then we will have a 3 values representing colors in triangle.
+And for 100 triangles we have array containing 900 values.
+
+To change DNA we can slightly change values of network.
+Each value change by some randomly choice value for example in range(0.01, 0.1).
+"""
